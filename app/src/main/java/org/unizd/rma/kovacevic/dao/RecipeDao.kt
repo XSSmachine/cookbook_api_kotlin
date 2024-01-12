21886599409cd049e394fc591c71737a4382decf
XSSmachine <kkovacevi21@student.unizd.hr>
2024-01-12 16:23:46 +0100

    Room implementation

---
+++ app/src/main/java/org/unizd/rma/kovacevic/dao/RecipeDao.kt
@@ -0,0 +1,18 @@
+package org.unizd.rma.kovacevic.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import org.unizd.rma.kovacevic.entitiy.Recipes
+
+@Dao
+interface RecipeDao {
+
+    @get:Query("SELECT * FROM recipes ORDER BY id DESC")
+    val allRecipes : List<Recipes>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun inserRecipe(recipes: Recipes)
+
+}
---
+++ app/src/main/java/org/unizd/rma/kovacevic/database/RecipeDatabase.kt
@@ -0,0 +1,32 @@
+package org.unizd.rma.kovacevic.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import org.unizd.rma.kovacevic.dao.RecipeDao
+import org.unizd.rma.kovacevic.entitiy.Recipes
+
+@Database(entities = [Recipes::class],version = 1,exportSchema = false)
+abstract class RecipeDatabase: RoomDatabase() {
+
+    companion object{
+
+        var recipesDatabase:RecipeDatabase? = null
+
+        @Synchronized
+        fun getDatabase(context: Context): RecipeDatabase{
+            if (recipesDatabase == null){
+                recipesDatabase = Room.databaseBuilder(
+                    context,
+                    RecipeDatabase::class.java,
+                    "recipe.db"
+                ).build()
+            }
+            return recipesDatabase!!
+        }
+    }
+
+    abstract fun recipeDao(): RecipeDao
+}
